1e32e0890562b78343adbeeda6eaae3f99e395a0
alameen <alameen.234516@gmail.com>
2025-12-06 14:44:24 +0100

    Define message types for server protocol

--- platform-fighter/public/server/protocol.js
+++ platform-fighter/public/server/protocol.js
@@ -1,1 +1,12 @@
-
+module.exports = {
+  MESSAGE_TYPES: {
+    JOIN: "join",
+    INPUT: "input",
+    READY: "ready",
+    WELCOME: "welcome",
+    SNAPSHOT: "snapshot",
+    COUNTDOWN: "countdown",
+    ROUND_END: "roundEnd",
+    ERROR: "error"
+  }
+};
